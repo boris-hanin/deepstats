ac90d22eef3102c71f96b7f852fc5a844367cec3
Boris Hanin <bhanin@gmail.com>
2017-03-05 08:59:17 -0500

    Remove output.

--- python_boilerplate/cli.py
+++ python_boilerplate/cli.py
@@ -6,10 +6,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for python_boilerplate"""
-    click.echo("Replace this message by putting your code into "
-               "python_boilerplate.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-
+    pass
 
 if __name__ == "__main__":
     main()
